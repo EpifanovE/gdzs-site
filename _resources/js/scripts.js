204d4786e2b0fa4f01a7c2962dd18dc34fcd73f3
Evgeny Epifanov <workeev@gmail.com>
2021-01-13 10:13:58 +0300

    add a settings help page

--- _resources/js/scripts.js
+++ _resources/js/scripts.js
@@ -1,1 +1,20 @@
 import {Dropdown} from 'bootstrap'
+
+jQuery(function($) {
+    const imageLinks = $(".ImageLink");
+    const imageModal = $("#imagemodal");
+    const imageModalImg = $("#imagemodal img")
+
+    if (imageLinks.length === 0 || imageModal.length === 0 || imageModalImg === 0) {
+        return;
+    }
+
+    imageLinks.each(function(e) {
+        $(this).on("click", function(e) {
+            const src = $(this).attr("src");
+            imageModalImg.attr("src", src);
+            imageModal.modal("show");
+        });
+    });
+
+});
